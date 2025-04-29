ba26e6430d7cc08599f00e6a64e15744b054627c
levi13 <30730638+levi13@users.noreply.github.com>
2025-04-29 20:15:09 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,32 @@ import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
 import 'screens/dashboard_screen.dart';
 import 'screens/imc_screen.dart';
-// Adicione outras telas conforme forem sendo criadas
+import 'screens/refeicoes_screen.dart';  
+import 'screens/atividades_screen.dart';
+
 
 void main() {
-  runApp(AppSaude());
+  runApp(const AppSaude());
 }
 
 class AppSaude extends StatelessWidget {
+  const AppSaude({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'App Saúde',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => LoginScreen(),
-        '/dashboard': (context) => DashboardScreen(),
-        '/imc': (context) => ImcScreen(),
-        // Adicione mais rotas aqui
+        '/': (context) => const LoginScreen(),
+        '/dashboard': (context) => const DashboardScreen(),
+        '/imc': (context) => const ImcScreen(),
+        '/refeicoes': (context) => const RefeicoesScreen(refeicao: {},), // Rota para a tela de refeições
+        '/atividades': (context) => const AtividadesScreen(),
       },
     );
   }
